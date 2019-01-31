669719c1459745615a49dcb85fe9383bc5fe3747
richardsforu <praveen.somireddy@gmail.com>
2019-01-31 12:47:57 +0530

    Conponent Design

---
+++ TypeScript/test.js
@@ -0,0 +1,8 @@
+var Employee = /** @class */ (function () {
+    function Employee() {
+        this.name = "Praveen";
+        this.age = 65;
+        this.names = new Array();
+    }
+    return Employee;
+}());
---
+++ TypeScript/test.ts
@@ -0,0 +1,10 @@
+class Employee{
+    name:string="Praveen";
+    age:number=65;
+    dynamicData:any;
+    names=new Array();
+    cities:[];
+    constructor(){
+
+    }
+}
